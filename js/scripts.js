a2fafddfe45a6b379651d252aef083e81a9a99e8
delM <dmanzueta@gmail.com>
2020-11-13 12:00:02 -0500

    Update scripts.js
    1.2: JavaScript Basics Part 1 Completed Task

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 let pokemonName = 'Bulbasaur';
-let pokemonList = [
+let pokemonList = [];
 
-];
+let pokemon = {
+    name: 'pokemonName',
+    height: 7,
+    types: ['grass','poison']
+
+};
+
+var pokemonList = [
+    'Bulbasaur',
+    'Ivysaur',
+    'Venusaur'
+]
+
+// 1.2: JavaScript Basics Part 1 Task
