c8e533603d598f1d091cfde20aa913b9548414df
delM <dmanzueta@gmail.com>
2020-11-16 14:26:30 -0500

    Update scripts.js
    Added new objects to arrays,
    added pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,38 @@
-let pokemonName = 'Bulbasaur';
-let pokemonList = [];
+let pokemonName = ['Bulbasaur'];
 
-let pokemon = {
-    name: 'pokemonName',
+let Bulbasaur = {
+    name: 'Bulbasaur',
     height: 7,
     types: ['grass','poison']
 
 };
 
-var pokemonList = [
-    'Bulbasaur',
-    'Ivysaur',
-    'Venusaur'
-]
+let pokemon2 = {
+    name: 'Ivysaur',
+    height: 1,
+    types: ['grass','poison']
+};
 
-// 1.2: JavaScript Basics Part 1 Task
+let pokemon3 = {
+    name: 'Venusar',
+    height: 2,
+    types: ['grass','poison']
+};
+
+let pokemon4 = {
+    name: 'Pikachu',
+    height: 0.4,
+    types: ['electric']
+};
+
+var pokemonList = [
+    pokemon1,
+    pokemon2,
+    pokemon3,
+    pokemon4
+];
+
+
+ 
+
+// 1.2: JavaScript Basics Part 1 Task - ** updated
