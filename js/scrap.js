0ddf8989271c2f1ae669dcde27c91d29f3905428
delM <dmanzueta@gmail.com>
2020-11-24 19:16:57 -0500

    js update
    Cleaned up js
    Added the clean code before addressing assignment 1.3

--- js/scrap.js
+++ js/scrap.js
@@ -3,22 +3,22 @@ var pokemonList = [];
 let pokemon1 = {
     name: 'Bulbasaur',
     height: 0.7,
-    types: ['grass', 'poison']
+    types: ['grass','poison']
 }
 
 let pokemon2 = {
     name: 'Ivysaur',
     height: 1,
-    types: ['grass', 'poison']
+    types: ['grass','poison']
 }
 
 let pokemon3 = {
     name: 'Venusaur',
     height: 2,
-    types: ['grass', 'poison']
+    types: ['grass','poison']
 }
 
-let pokemon4 = {
+let pokemon4  = {
     name: 'Charmander',
     height: 0.6,
     types: ['fire']
