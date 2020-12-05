46227882906e1f13f3beeb995a37018754c6e40f
delM <dmanzueta@gmail.com>
2020-12-04 19:26:26 -0500

    Update scripts.js
    updated pokemonList array,
    replaced for loop with forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,63 @@
-let pokemon1 = {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['grass', 'poison']
-}
 
-let pokemon2 = {
-    name: 'Ivysaur',
-    height: 1,
-    types: ['grass', 'poison']
-}
-
-let pokemon3 = {
-    name: 'Venusaur',
-    height: 2,
-    types: ['grass', 'poison']
-}
-
-let pokemon4 = {
-    name: 'Charmander',
-    height: 0.6,
-    types: ['fire']
-}
-
-let pokemon5 = {
-    name: 'Charmeleon',
-    height: 1.1,
-    types: ['fire']
-}
-
-let pokemon6 = {
-    name: 'Charizard',
-    height: 1.7,
-    types: ['fire' , 'flying']
-}
-
-let pokemon7 = {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ['water']
-}
-
-let pokemon8 = {
-    name: 'Wartortle',
-    height: 1,
-    types: ['water']
-}
-
-let pokemon9 = {
-    name: 'Balstoise',
-    height: 1.6,
-    types: ['water']
-}
-
-var pokemonList = [
-    pokemon1,
-    pokemon2,
-    pokemon3,
-    pokemon4,
-    pokemon5,
-    pokemon6,
-    pokemon7,
-    pokemon8,
-    pokemon9
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', ' poison'],
+        fire: false
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        types: ['grass', ' poison'],
+        fire: false
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        types: ['grass', ' poison'],
+        fire: false
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['fire'],
+        fire: true
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1,
+        types: ['fire'],
+        fire: true
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['fire', 'flying'],
+        fire: true
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        types: ['water'],
+        fire: true
+    },
+    {
+        name: 'Wartortle',
+        height: 1,
+        types: ['water'],
+        fire: true
+    },
+    {
+        name: 'Balstoise',
+        height: 1.6,
+        types: ['water'],
+        fire: true
+    },
 ];
 
 
-// for (var pokemon in pokemonList) {
-//     document.write('<h3>Name: </h3>' + '<p>' + pokemonList[pokemon].name + ' </p> ' + ' <p>Height:' + pokemonList[pokemon].height + ' </p> ' + '<p>Types: ' + pokemonList[pokemon].types + '</p>')
-    
-// }
+pokemonList.forEach(function (pokemonType) {
+    document.write('<p><strong>Name: </strong></p> ' + '<p>' + pokemonType.name + '<br />' + '[Height: ' + pokemonType.height + ']' + '<br>' + '[Types: ' + pokemonType.types + ']' + '</p>');
 
-//added more pokemon characters to list
+}); 
