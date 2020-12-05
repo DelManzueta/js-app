3ed55bedc036a6fa99f9de1263c0c65bab10b76f
Del <dmanzueta@gmail.com>
2020-12-04 23:08:32 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,83 @@
+var pokemonRepository = (function (pokemonArray) {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            types: ['grass', ' poison'],
+            fire: false
+        },
+        {
+            name: 'Ivysaur',
+            height: 1,
+            types: ['grass', ' poison'],
+            fire: false
+        },
+        {
+            name: 'Venusaur',
+            height: 2,
+            types: ['grass', ' poison'],
+            fire: false
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            types: ['fire'],
+            fire: true
+        },
+        {
+            name: 'Charmeleon',
+            height: 1.1,
+            types: ['fire'],
+            fire: true
+        },
+        {
+            name: 'Charizard',
+            height: 1.7,
+            types: ['fire', 'flying'],
+            fire: true
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            types: ['water'],
+            fire: true
+        },
+        {
+            name: 'Wartortle',
+            height: 1,
+            types: ['water'],
+            fire: true
+        },
+        {
+            name: 'Balstoise',
+            height: 1.6,
+            types: ['water'],
+            fire: true
+        },
+    ];
 
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', ' poison'],
-        fire: false
-    },
-    {
-        name: 'Ivysaur',
-        height: 1,
-        types: ['grass', ' poison'],
-        fire: false
-    },
-    {
-        name: 'Venusaur',
-        height: 2,
-        types: ['grass', ' poison'],
-        fire: false
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        types: ['fire'],
-        fire: true
-    },
-    {
-        name: 'Charmeleon',
-        height: 1.1,
-        types: ['fire'],
-        fire: true
-    },
-    {
-        name: 'Charizard',
-        height: 1.7,
-        types: ['fire', 'flying'],
-        fire: true
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        types: ['water'],
-        fire: true
-    },
-    {
-        name: 'Wartortle',
-        height: 1,
-        types: ['water'],
-        fire: true
-    },
-    {
-        name: 'Balstoise',
-        height: 1.6,
-        types: ['water'],
-        fire: true
-    },
-];
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+
+})();
 
 
-pokemonList.forEach(function (pokemonType) {
+
+
+
+/*
+pokemonRepository.forEach(function (pokemonType) {
     document.write('<p><strong>Name: </strong></p> ' + '<p>' + pokemonType.name + '<br />' + '[Height: ' + pokemonType.height + ']' + '<br>' + '[Types: ' + pokemonType.types + ']' + '</p>');
 
-}); 
+});
+ */
